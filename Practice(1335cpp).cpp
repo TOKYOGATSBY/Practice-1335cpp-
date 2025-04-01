2331d0c84c6125c81e6ccc123be5f2dd5a06886a
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-01 12:08:12 +0300

    version 1.1

--- Practice(1335cpp).cpp
+++ Practice(1335cpp).cpp
@@ -1,7 +1,34 @@
 ﻿#include <iostream>
+#include <unordered_map>
+using namespace std;
 
-int main()
-{
-    
+int main() {
+    int N;
+    cin >> N;
+
+    int low = N * N;
+    int high = (N + 1) * (N + 1);
+
+    for (int C = low; C <= high; ++C) {
+        unordered_map<int, int> square_mod;
+        for (int A = low; A <= high; ++A) {
+            int mod = (1LL * A * A) % C;
+            square_mod[mod] = A;
+        }
+
+        for (int B = low; B <= high; ++B) {
+            int mod_b = (1LL * B * B) % C;
+            int target = (C - mod_b) % C;
+            if (square_mod.count(target)) {
+                int A = square_mod[target];
+                if (A != B && A != C && B != C) {
+                    cout << A << " " << B << " " << C << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "No solution" << endl;
+    return 0;
 }
-
